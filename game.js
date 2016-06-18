d09f0a18300ad682a0145635ec1f95390ba6cbe2
Charlie Fish <fishcharlie@me.com>
2016-06-18 09:40:47 -0600

    Creating checkView function to reset game board = to game array variable

--- game.js
+++ game.js
@@ -3,5 +3,13 @@
 var gameArray = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
 var whosTurn = 'X';
 start();
+
+function checkView(gameArray) {
+	for (var x = 0; x < gameArray.length; x++) {
+		for (var y = 0; y < gameArray[x].length; y++) {
+			setCell(gameArray[x][y], x, y);
+		}
+	}
+}
 function start() {
 }
