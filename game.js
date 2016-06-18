14b1fe2857a882a36ea2ae76c70639c073e2905d
Charlie Fish <fishcharlie@me.com>
2016-06-18 10:15:58 -0600

    Adding logic for win state and styling for winner tiles

--- game.js
+++ game.js
@@ -2,6 +2,7 @@
 
 var gameArray = [['.', '.', '.'], ['.', '.', '.'], ['.', '.', '.']];
 var whosTurn = 'X';
+var winnerStatus = false;
 start();
 
 function checkView(gameArray) {
@@ -37,13 +38,19 @@ function clearCell(x, y) {
 	if (cell.classList.contains('o')) {
 		cell.classList.remove('o');
 	}
+	if (cell.classList.contains('winnerCell')) {
+		cell.classList.remove('winnerCell');
+	}
 	cell.textContent = '';
 }
 
 function move(id) {
-	if (gameArray[id[0]][id[1]] == '.') {
-		setCell(whosTurn,id[0],id[1]);
-		changeMove();
+	if (winnerStatus === false) {
+		if (gameArray[id[0]][id[1]] == '.') {
+			setCell(whosTurn,id[0],id[1]);
+			changeMove();
+			checkBoard();
+		}
 	}
 }
 
@@ -64,6 +71,59 @@ function clearBoard() {
 	whosTurn = 'X';
 	var infoText = document.getElementsByClassName('infoText')[0];
 	infoText.textContent = whosTurn + '\'s Turn!';
+	winnerStatus = false;
+}
+
+function checkBoard() {
+	var tile00 = document.getElementById('00');
+	var tile01 = document.getElementById('01');
+	var tile02 = document.getElementById('02');
+	var tile10 = document.getElementById('10');
+	var tile11 = document.getElementById('11');
+	var tile12 = document.getElementById('12');
+	var tile20 = document.getElementById('20');
+	var tile21 = document.getElementById('21');
+	var tile22 = document.getElementById('22');
+
+	if (tile00.textContent == tile10.textContent && tile00.textContent == tile20.textContent && tile00.textContent !== '') {
+		winner(tile00.textContent, ['00', '10', '20']);
+	}
+	else if (tile01.textContent == tile11.textContent && tile01.textContent == tile21.textContent && tile01.textContent !== '') {
+		winner(tile01.textContent, ['01', '11', '21']);
+	}
+	else if (tile02.textContent == tile12.textContent && tile02.textContent == tile22.textContent && tile02.textContent !== '') {
+		winner(tile02.textContent, ['02', '12', '22']);
+	}
+	else if (tile00.textContent == tile01.textContent && tile00.textContent == tile02.textContent && tile00.textContent !== '') {
+		winner(tile00.textContent, ['00', '01', '02']);
+	}
+	else if (tile10.textContent == tile11.textContent && tile10.textContent == tile12.textContent && tile10.textContent !== '') {
+		winner(tile10.textContent, ['10', '11', '12']);
+	}
+	else if (tile20.textContent == tile21.textContent && tile20.textContent == tile22.textContent && tile20.textContent !== '') {
+		winner(tile20.textContent, ['20', '21', '22']);
+	}
+	else if (tile00.textContent == tile11.textContent && tile00.textContent == tile22.textContent && tile00.textContent !== '') {
+		winner(tile00.textContent, ['00', '11', '22']);
+	}
+	else if (tile02.textContent == tile11.textContent && tile02.textContent == tile20.textContent && tile02.textContent !== '') {
+		winner(tile02.textContent, ['02', '11', '20']);
+	}
+}
+
+function winner(winnerXO, arrayOfWinnerTiles) {
+	winnerStatus = true;
+
+	var tile1 = document.getElementById(arrayOfWinnerTiles[0]);
+	var tile2 = document.getElementById(arrayOfWinnerTiles[1]);
+	var tile3 = document.getElementById(arrayOfWinnerTiles[2]);
+
+	tile1.classList.add('winnerCell');
+	tile2.classList.add('winnerCell');
+	tile3.classList.add('winnerCell');
+
+	var infoText = document.getElementsByClassName('infoText')[0];
+	infoText.textContent = winnerXO + ' wins!!!';
 }
 
 function start() {
